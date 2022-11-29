565d421506860f5bff8ff73bff8662d0cdf40fe6
Kazi Reazul Islam <kazireazulislam1999@gmail.com>
2022-11-29 11:40:46 +0600

    Rating Added

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,11 +7,13 @@ const HomeScreen = () => {
     <>
       <h1>Latest Products</h1>
       <Row>
-        {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
+        {products.map((product) => {
+          return (
+            <Col key={product?._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          );
+        })}
       </Row>
     </>
   );
